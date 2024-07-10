b1ecc1868e69cd08cfcb7ed4778a9c26bac79f00
mkarlthayer <mkarlthayer@gmail.com>
2024-07-10 10:07:01 -0700

    Test

--- stockVisuals/stockVisuals/settings.py
+++ stockVisuals/stockVisuals/settings.py
@@ -27,7 +27,7 @@ FRONTEND_DIR = BASE_DIR.parent / 'frontend'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1']
 
--- stockVisuals/stockVisuals/urls.py
+++ stockVisuals/stockVisuals/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from visualizer.views import stockAPIView
 
 urlpatterns = [
-    path('visualizer/', stockAPIView.as_view(), name = "visualizerData"), # go straight to visualizer application
+    path('', stockAPIView.as_view(), name = "visualizerData"), # go straight to visualizer application
     path('admin/', admin.site.urls),
 ]
