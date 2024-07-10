3994e3cf23f29b1fe413b25252cbffd07c365dc9
mkarlthayer <mkarlthayer@gmail.com>
2024-07-10 09:14:02 -0700

    updates for production

--- stockVisuals/stockVisuals/settings.py
+++ stockVisuals/stockVisuals/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ FRONTEND_DIR = BASE_DIR.parent / 'frontend'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = mySecretKey
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
