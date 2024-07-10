eb7fd8049511f3fdf30846cb1949a605b3d2888f
mkarlthayer <mkarlthayer@gmail.com>
2024-07-10 12:09:31 -0700

    Update for Production

--- stockVisuals/stockVisuals/settings.py
+++ stockVisuals/stockVisuals/settings.py
@@ -27,7 +27,7 @@ FRONTEND_DIR = BASE_DIR.parent / 'frontend'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
