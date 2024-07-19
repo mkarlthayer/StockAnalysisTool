57171e1006fc2a1f003fc0c5cacffcea8d4f4eb8
mkarlthayer <mkarlthayer@gmail.com>
2024-07-19 12:27:03 -0700

    Added post tests

--- stockVisuals/visualizer/tests.py
+++ stockVisuals/visualizer/tests.py
@@ -15,4 +15,14 @@ class StockAPITestCase(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_post_invalid_input(self):
+        response = self.client.post(self.url, {'symbol': self.invalid_symbol}, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('error', response.data)
+
+    def test_post_valid_input(self):
+        response = self.client.post(self.url, {'symbol': self.valid_symbol}, format = 'json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
         
